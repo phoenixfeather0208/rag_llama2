1a5066966f17442191d0087756df80dfc3f1a572
phoenixfeather0208 <phoenixfeather0208@gmail.com>
2025-01-22 10:20:17 -0400

    get full response from chunk resopnse

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import faiss
 import numpy as np
+import requests
+import json
 
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+url = "http://localhost:11434/api/generate"
 
 documents = [
     "The Eiffel Tower is located in Paris, France.",
@@ -17,15 +20,11 @@ dimension = embeddings.shape[1]
 index = faiss.IndexFlatL2(dimension)
 index.add(np.array(embeddings).astype("float32"))
 
-model_name = ""
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
 user_query = "Where is the Eiffel Tower located?"
 
-query_embedding = embedding_model.encode(user_query)
+query_embedding = embedding_model.encode([user_query])
 
-k = 2
+k = 2  # Number of documents to retrieve
 distances, indices = index.search(np.array(query_embedding).astype("float32"), k)
 retrieved_docs = [documents[i] for i in indices[0]]
 context = "\n".join(retrieved_docs)
@@ -34,8 +33,20 @@ print(f"Retrieved Docs: {retrieved_docs}")
 
 input_prompt = f"Context: {context}\n\nUser Query: {user_query}\n\nAnswer:"
 
-inputs = tokenizer(input_prompt, return_tensors="pt").to("cpu") #cuda
-outputs = model.generate(inputs["input_ids"], max_length=200, num_beams=5, no_repeat_ngram_size=2)
-response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+payload = {
+    "model": "llama2:latest",
+    "prompt": "".join(input_prompt)
+}
 
-print(f"\nGenerated Response:\n {response}")
+response = requests.post(url, json=payload)
+full_response = ""
+if response.status_code == 200:
+    for chunk in response.iter_lines(decode_unicode=True):
+        if chunk:
+            # Parse each line (JSON object) and extract the "response" field
+            data = json.loads(chunk)
+            full_response += data.get("response", "")
+    
+    print("Complete Response:", full_response)
+else:
+    print(response.status_code)
